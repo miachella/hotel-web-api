388db02520d1a497a94e182199191070bff44c34
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-09-07 16:22:15 +0200

    Web API / GET / clients?start=X&size=Y tests

--- src/main/java/dev/hotel/web/Controller.java
+++ src/main/java/dev/hotel/web/Controller.java
@@ -1,8 +1,11 @@
 package dev.hotel.web;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +27,16 @@ public class Controller {
 	@RequestMapping(method = RequestMethod.GET, path = "clients")
 	public List<Client> listClient(@RequestParam int start, @RequestParam int size) {
 		return clientRepository.findAll(PageRequest.of(start, size)).getContent();
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "clients/{UUID}")
+	public ResponseEntity<?> getClientUUID(@RequestParam UUID UUID) {
+		if (clientRepository.findById(UUID).isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).header("message", "cool").body(clientRepository.findById(UUID));
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Veuillez entrer un autre identifiant client");
+		}
 
 	}
 
---
+++ src/test/java/dev/hotel/web/ControllerTest.java
@@ -0,0 +1,45 @@
+package dev.hotel.web;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import dev.hotel.entite.Client;
+import dev.hotel.repository.ClientRepository;
+
+@WebMvcTest(Controller.class)
+public class ControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ClientRepository clientRepository;
+
+	@Test
+	void testListClient() throws Exception {
+
+		Client client = new Client("Boop", "Betty");
+		Client client2 = new Client("Rabbit", "Roger");
+
+		Mockito.when(clientRepository.findAll(PageRequest.of(0, 2)))
+				.thenReturn(new PageImpl<>(Arrays.asList(client, client2)));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/clients?start=0&size=2")).andExpect(status().isOk())
+				.andExpect(jsonPath("[0].nom").value("Boop")).andExpect(jsonPath("[0].prenoms").value("Betty"))
+				.andExpect(jsonPath("[1].nom").value("Rabbit")).andExpect(jsonPath("[1].prenoms").value("Roger"));
+
+	}
+
+}
